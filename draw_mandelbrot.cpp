34af2fd73ab702753ff3b2cba83e103b7bfa277a
hegek87 <hegelandka@vcu.edu>
2014-01-26 15:18:07 -0500

    Small updates

--- draw_mandelbrot.cpp
+++ draw_mandelbrot.cpp
@@ -1,13 +1,7 @@
 #include "mandelbrot.hpp"
 
 int main(int argc, char **argv){
-	char *fileName;
-	if(argc == 2){
-		fileName = argv[1];
-	}
-	else{
-		fileName = (char *)"Mandelbrot.bmp";
-	}
+	char *fileName = ((argc==2) ? argv[1] : (char *)"Mandelbrot.bmp");
 	Mandelbrot mb(TOLERANCE, WIDTH, HEIGHT);
 	mb.generateImage();
 	mb.getBitmap().createBMP(fileName);	
--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -14,7 +14,7 @@ static int outSet = 0;
 *  We will return a color based on the value of count after
 *  the loop terminates. If count >= tolerance, return BLACK, 
 *  otherwise we will return a color based on count. We use 
-*  log properties to make the color change fade.
+*  log properties to make the color change fade smoothly.
 */
 Color Mandelbrot::iteratePoly(Complex point){
 	Complex polyDat[3] = {point,Complex(0,0),Complex(1,0)};
